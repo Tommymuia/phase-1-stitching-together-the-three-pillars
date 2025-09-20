60ce3540888a237ec1b57e597ad9ea0f32434848
Tommymuia <muiathomas.mt@gmail.com>
2025-09-20 13:15:14 +0300

    added

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 /* This is a file inserted to keep the learn IDE browser happy */
-const testVar = {}
+//const testVar = {}
+// index.js
+
+// Declare testVar as an empty object
+const testVar = {};
+
+// Expose to window for testing
+window.testVar = testVar;
